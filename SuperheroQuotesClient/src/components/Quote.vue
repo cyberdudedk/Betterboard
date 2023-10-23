41a0e1b15de6e4f386fe9f5220b934b4f73aef17
Per Bellfield <per.bellfield@gmail.com>
2023-10-23 18:40:26 +0200

    Fetchdata

--- SuperheroQuotesClient/src/components/Quote.vue
+++ SuperheroQuotesClient/src/components/Quote.vue
@@ -1,10 +1,31 @@
 <template>
   <div class="superhero-quote">
     <div class="quote">
-      Quote
+      {{ heroquote.quote }}
     </div>
     <span class="superhero">
-      - Batman
+      - {{ heroquote.name }}
     </span>
   </div>
 </template>
+
+<script  lang="ts">
+export default {
+  data() {
+    return {
+      heroquote: {} as { quote: string, name: string }
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.heroquote = {
+        quote: "This is a quote",
+        name: "Superman",
+      };
+    },
+  },
+};
+</script>
