12a559244d45ee7706c2a3de0983fc539e87d829
Per Bellfield <per.bellfield@gmail.com>
2023-10-23 19:11:23 +0200

    Load quotes

--- SuperheroQuotesServer/QuotesService.cs
+++ SuperheroQuotesServer/QuotesService.cs
@@ -1,7 +1,22 @@
 
+using System.Text.Json;
+
 internal class QuotesService
 {
     private static Random random = new Random();
+
+    private readonly IEnumerable<Quote> Quotes = InitializeQuotes<Quote>();
+
+    private static IEnumerable<T> InitializeQuotes<T>() {
+        
+        string text = File.ReadAllText("quotes.json");
+        IEnumerable<T>? result = JsonSerializer.Deserialize<IEnumerable<T>>(text);
+
+        if (result == null)
+            throw new Exception("Deserialization returned null");
+
+        return result;
+    }
 
     internal Quote GetRandomQuote()
     {
